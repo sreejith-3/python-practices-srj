1127915e87f51fcda75a71c8714dd15375432451
unknown <sreejith.cloudme@gmail.com>
2023-08-23 10:20:59 +0530

    new don

--- String-Manupulation/string-manupulation.py
+++ String-Manupulation/string-manupulation.py
@@ -6,7 +6,12 @@ print("This is the first line !!\nThis is the second line !!")
 print("Hello" + "Angela")
 # The Output will look like > HelloAngela
 
-# Next Scenario
+# Next Scenario 1
 print("Hello" + " Angela")
 # The Output will look like > Hello Angela
 
+# Next Scenario  2- you may use this 
+print("Hello " + "Angela")
+
+# Next Scenario 3 - you may use this 
+print("Hello " + "" + "Angela")
