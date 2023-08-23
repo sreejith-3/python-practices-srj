25d2b435063058034b865f953730ccb1cd84b8d4
unknown <sreejith.cloudme@gmail.com>
2023-08-23 10:18:12 +0530

    added a new one

--- String-Manupulation/string-manupulation.py
+++ String-Manupulation/string-manupulation.py
@@ -1,3 +1,12 @@
 print("Hello World !!\nHello World !!")
 print("")
 print("This is the first line !!\nThis is the second line !!")
+
+# Next Scenario
+print("Hello" + "Angela")
+# The Output will look like > HelloAngela
+
+# Next Scenario
+print("Hello" + " Angela")
+# The Output will look like > Hello Angela
+
